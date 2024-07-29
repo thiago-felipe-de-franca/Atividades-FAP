84920bbd28cc90c967f442628b72cac465d23719
thiago-felipe-de-franca <thiagofdefranca@gmail.com>
2024-07-29 10:55:28 -0300

    Commit da atividade 05

---
+++ Atividade 05/index.ts
@@ -0,0 +1,125 @@
+// Exercício 01
+// R-> o comando for serve para criar um laço de repetição onde sao realizadas iterações de acordo com o desejo do usuário
+
+// Exercício 02
+// R-> o comando for possui um teste condicional se essa condicção for verdadeira a iteração acontece e o laço se inici
+
+// Exercício 03
+// R-> Se o teste condicional for falso o laço se encerra
+
+// Exercício 04
+// R-> A o final da iteração e da execução do bloco de código o indíce recebe uma alteração que geralmente é de  +1 através do código "++".
+
+// Exercício 05
+// for (let index = 0; index < 10; index++) {
+//     console.log("Testando uma frase!")
+// }
+
+// Exercício 06
+// for (let i = 0; i < 10; i++) {
+//     console.log(i)
+    
+// }
+
+// Exercício 07
+// for (let i = 0; i < 10; i++) {
+//     if(i == 5) {
+//         break
+//     }
+//     console.log(i)
+// }
+
+// Exercício 08
+// for (let i = 0; i < 10; i++) {
+//    if (i == 5) {
+//     continue
+//    }
+//    console.log(i)
+// }
+
+// Exercício 09
+// let nomes: string[] = [
+//     'João',
+//     'Paulo',
+//     'Pedro',
+//     'Gustavo',
+//     'Maria'
+// ]
+
+// for (let n of nomes) {
+//     console.log(n)
+// }
+
+// Exercício 10
+// for (let index = 0; index < 10; index++) {
+//     console.log(index + 1)
+// }
+
+// Exercício 11
+// for (let index = 10; index > 0; index-- ) {
+//     console.log(index)
+// }
+
+// Exercício 12
+// let resultado:number = 0
+
+// for (let index = 0; index < 101; index++) {
+//     resultado += index
+// }
+
+// console.log(resultado)
+
+// Exercício 13
+// for (let index = 1; index <= 50; index++) {
+//     if (index % 2 === 0) {
+//     console.log(index)
+//     }
+// }
+
+// Exercício 14
+
+
+// Exercício 15
+// let fatorial_5:number = 1
+
+// for (let index = 1; index < 6; index++) {
+//     fatorial_5 *= index
+// }
+
+// console.log(fatorial_5)
+
+// Exercício 16
+let somatorio:number = 0
+let listaDeNotas:number[] = [
+
+]
+for (let index = 0; index <= 5; index++) {
+    let nota = prompt("Digite a nota:")
+    listaDeNotas.push(Number(nota))
+}
+
+for (let index = 0; index <= 5; index++) {
+    somatorio += listaDeNotas[index]
+}
+const media: number = somatorio / 5
+console.log(media)
+
+// Exercício 17
+
+// Exercício 18
+
+// Exercício 19
+
+// Exercício 20
+
+// Exercício 21
+
+// Exercício 22
+
+// Exercício 23
+
+// Exercício 24
+
+// Exercício 25
+
+// Exercício 26
