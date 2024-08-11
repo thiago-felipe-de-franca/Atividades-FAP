d2b803fda426b01a5665ffda08d310da06f6fa81
thiago-felipe-franca <thiagofdefranca@gmail.com>
2024-08-11 16:53:31 -0300

    Reorganizando pastas e commit atividade HTML 01

---
+++ Atividades/HTML/Atividade 01/Index.ts
@@ -0,0 +1,62 @@
+// Conhecendo o HTML e sua estrutura básica
+
+// Exercicio 1
+// R-> O HTML ou hypertext markup language e uma lingaguem de marcacao e estruturacao de texto que vai ser de corpo para paginas web
+// 
+// Exercicio 2
+// R-> O HTML serve como estrutura basica e esqueleto da pagina web, e atraves de suas marcacoes que podemos estruturar o site
+// 
+
+// Identificando tags HTML básicas
+
+// Exercicio 1
+// R-> <p>: tag p e utilizada para paragrafos
+//     <div> : tag div serve para fazer divisoes no codigo
+//     <body>: tag body e onde fica o corpo do codigo
+
+// Exercicio 2
+// R->As tags <ul> e <ol> servem para criar listas onde o objetivo e colocar de maneira linear um conjunto de valores, <ol> ou ordered list e uma boa opcao para numerar uma sequencia de passos como por exemplo uma receita, ja a tag <ul> ou unordered list serve para quando a ordem dos valores nao e tao relevante como por exemplo numa lista de compras
+// 
+
+// Vinculando páginas, arquivos e âncoras
+
+// Exercicio 1
+// R-> A tag <a> cria links em HTML, usando o atributo href para especificar o destino. É útil para vincular páginas, arquivos ou âncoras dentro da mesma página. O atributo target controla como o link é aberto, por exemplo, em uma nova aba usando _blank.
+// 
+// Exercicio 2
+// R-> Além de href, a tag <a> pode ter atributos como title e rel. O atributo target determina como o link é exibido, permitindo, por exemplo, abrir em uma nova janela com _blank.
+// 
+
+// Incorporando imagens e mídias
+
+// Exercicio 1
+// R-> Os atributos src e alt na tag <img> definem a fonte da imagem e um texto alternativo, respectivamente. O alt é crucial para acessibilidade e SEO.
+// 
+// Exercicio 2
+// R-> Além de imagens, mídias como vídeos e áudios podem ser incorporados com <video> e <audio>. Para garantir responsividade, deve-se usar atributos como width e height em conjunto com CSS responsivo.
+// 
+
+// Criando tabelas básicas
+
+// Exercicio 1
+// R-> As principais tags para tabelas são <table>, <tr>, <th> e <td>. <th> define cabeçalhos e <td> define células de dados; <th> é usada para destacar títulos ou categorias.
+// 
+// Exercicio 2
+// R-> Tabelas podem ser complexas e difíceis de adaptar a diferentes dispositivos, por isso, outras formas de apresentação como listas ou grids com CSS são preferidas para layouts. No entanto, as tabelas são apropriadas para organizar dados numéricos ou informações que necessitam de uma estrutura tabular clara, como planilhas e comparações de preços.
+// 
+// Criando formulários
+
+// Exercicio 1
+// R-> Embora tabelas sejam úteis para dados tabulares, deve-se evitar usá-las para layout, preferindo CSS. Tabelas são apropriadas quando os dados têm uma estrutura tabular clara.
+// 
+// Exercicio 2
+// R-> A tag <form> cria formulários, e diferentes tipos de campos de entrada podem ser usados com <input>, como text, password e email. Outros elementos, como <button> e <label>, ajudam na usabilidade do formulário.
+// 
+// Técnicas avançadas de estruturação com elementos HTML5 semânticos
+
+// Exercicio 1
+// R-> Elementos HTML5 semânticos, como <header>, <footer> e <section>, são importantes para estruturar o conteúdo da página de maneira significativa, melhorando a acessibilidade e SEO.
+// 
+// Exercicio 2
+// R-> Elementos HTML5 semânticos ajudam na acessibilidade, pois descrevem melhor a estrutura do conteúdo para leitores de tela e motores de busca. Considerar a acessibilidade é fundamental para uma web inclusiva.
+// 
---
+++ Atividades/Typescript/Atividade 06/index.js
@@ -0,0 +1,267 @@
+// Exercício 01
+// R-> For possui um ciclo definido de iteracoes determinado pelo seu teste condicional, for possui um indice proprio ja na sua sintaxe, while nao sabemos ao certo quantas iteracoes serao necessarias para se encerrar o laco de repeticao e precisamos implementar um contador proprio que ira ditar quando o laco deve ser encerrado 
+// Exercício 02
+// R-> No geral utilizamos For quando ja conhecemos quantas iteracoes nosso codigo devera realizar enquanto while utilizamos quando nao sabemos ao certo quantas iteracaoes serao necessarias
+// Exercício 03
+// let i: number = 0
+// while (i < 10) {
+//     console.log("Testando uma frase!")
+//     i++
+// }
+// Exercício 04
+// let i:number = 465484133
+// while (i > 10) {
+//     console.log(i)
+//     i /= 35
+// }
+// Exercício 05
+// let i: number = 0
+// // do {
+// //     console.log(i)
+// //     i++
+// // } while (i < 5);
+// Exercício 06
+// let index: number = 1
+// while (index <= 10) {
+//     console.log(index)
+//     index++
+// }
+// Exercício 07
+// let index: number = 10
+// while (index >= 1) {
+//     console.log(index)
+//     index--
+// }
+// Exercício 08
+// let index: number = 1
+// let resultado: number = 0
+// while (index <= 100) {
+//     resultado += index
+//     index++
+//     console.log(resultado)
+// }
+// Exercício 09
+// let index: number = 1
+// let fatorial: number = 1
+// while (index <= 5) {
+//     fatorial *= index
+//     index++
+//     console.log(fatorial)
+// }
+// Exercício 10
+// let index:number = 0
+// console.log(`Tabuada de 9:`)
+// while (index <= 10) {
+//     console.log(`9 X ${index} = ${9*index}`)
+//     index++
+// }
+// Exercício 11
+// let index = Number(prompt("Digite um numero: "))
+// let listaNumeros: number[] = []
+// if (index == 0) {
+//     alert("Lista vazia!")
+// } else {
+//     listaNumeros.push(index)
+// }
+// while (index !== 0) {
+//     index = Number(prompt("Digite um numero: "))
+//     if (index !== 0) {
+//         listaNumeros.push(index)
+//     }
+// }
+// if (listaNumeros.length > 0) {
+//     let varTeste: number = listaNumeros[0]
+//     let maior: number = listaNumeros[0]
+//     let menor: number = listaNumeros[0]
+//     for (let index = 0; index < listaNumeros.length; index++) {
+//         varTeste = listaNumeros[index]
+//         if (varTeste >= maior) {
+//             maior = varTeste
+//         }
+//         if (varTeste <= menor) {
+//         } menor = varTeste
+//     }
+//     alert(`O maior valor fornecido foi: ${maior}\nO menor valor fornecido foi: ${menor}`)
+// }
+// Exercício 12
+// let index = Number(prompt("Digite um numero: "))
+// let listaNumeros: number[] = []
+// if (index == -1) {
+//     alert("Lista vazia!")
+// } else {
+//     listaNumeros.push(index) 
+// }
+// while (index !== -1) {
+//     index = Number(prompt("Digite um numero: "))
+//     if (index !== -1) {
+//         listaNumeros.push(index)
+//     }
+// }
+// if (listaNumeros.length > 0) {
+//     let soma:number = 0
+//     for (let index = 0; index < listaNumeros.length; index++) {
+//         soma += listaNumeros[index]
+//     }
+//     let media: number = soma / listaNumeros.length
+//     alert(`A media foi: ${media.toFixed(2)}`)
+// }
+// Exercício 13
+// let numero = prompt(`Digite um numero: `)
+// let numeroArray: string[] = numero.split('')
+// let resultado: number = 0
+// for (let index = 0; index < numeroArray.length; index++) {
+//     resultado += (Number(numeroArray[index])) ** 3
+// }
+// alert(`Resultado: ${resultado}`)
+// 
+// Exercício 14
+// let index: number = 1
+// let indexArray: number[] = []
+// while (index <= 100) {
+//     if (index % 2 == 1) {
+//         indexArray.push(index)
+//     }
+//     index++
+// }
+// console.log(indexArray)
+// Exercício 15
+// let index: number = 1
+// let indexArray: number[] = []
+// while (index <= 50) {
+//     if (index % 3 == 0) {
+//         indexArray.push(index)
+//     }
+//     index++
+// }
+// console.log(indexArray)
+// Exercício 16
+// let listaDeAprovados: number[] = []
+// let index: number = 0
+// while (index < 5) {
+//     let nota = Number(prompt(`Digite uma nota:`))
+//     if (nota >= 7){
+//         listaDeAprovados.push(nota)
+//     }
+//     index++
+// }
+// alert(`${listaDeAprovados.length} alunos foram aprovados`)
+// Exercício 17
+// let numero = Number(prompt(`Digite um numero inteiro: `))
+// let numerosParesArray: number[] = []
+// if (numero == 0) {
+//     alert(`Programa encerrado!`)
+// } else {
+//     numerosParesArray.push(numero)
+// }
+// let index: number = 0
+// while (numero !== 0) {
+//     numero = Number(prompt(`Digite um numero inteiro: `))
+//     if (numero % 2 == 1) {
+//         index++
+//     } else if (index < 1){
+//          numerosParesArray.push(numero)
+//     }
+// }
+// alert(`Foram digitados ${numerosParesArray.length} numeros pares antes do primeiro numero impar!`)
+// Exercício 18
+// let numero = Number(prompt(`Digite um numero`))
+// let paresArray: number[] = []
+// let imparesArray: number[] = []
+// if (numero == 0) {
+//     alert(`Programa encerrado!`)
+// } else if (numero % 2 == 0) {
+//     paresArray.push(numero)
+// } else if (numero % 2 == 1) {
+//     imparesArray.push(numero)
+// }
+// if (paresArray.length > 0 || imparesArray.length > 0) {
+//     while (numero !== 0) {
+//         numero = Number(prompt(`Digite um numero`))
+//         if (numero !== 0) {
+//             if (numero % 2 == 0){
+//                 paresArray.push(numero)
+//             } else {
+//                 imparesArray.push(numero)
+//             }
+//         }
+//     }
+//     alert(`Foram digitados ${paresArray.length} numeros pares e ${imparesArray.length} numeros impares!`)
+// }
+// Exercício 19
+// let numero = Number(prompt())
+// let contador2: number = 0
+// let contador3: number = 0
+// let contador5: number = 0
+// let listaNumeros: number[] = []
+// if (numero == 0) {
+//     alert(`Programa encerrado`)
+// } else {
+//     listaNumeros.push(numero)
+//     while (numero !== 0 ) {
+//         numero = Number(prompt())
+//         if (numero !== 0) {
+//             listaNumeros.push(numero)
+//         }
+//     }
+//     let index: number = 0
+//     while (index < listaNumeros.length) {
+//         if (listaNumeros[index] % 2 == 0) {
+//             contador2++
+//         }
+//         if (listaNumeros[index] % 3 == 0){
+//             contador3++
+//         }
+//         if (listaNumeros[index] % 5 == 0){
+//             contador5++
+//         }
+//         index++
+//     }
+//     alert(`Resultado: ${contador2} numeros sao divisiveis por 2.\n${contador3} numero divisiveis por 3.\n${contador5} numeros divisiveis por 5.`)
+// }
+// Exercício 20
+// let numero = Number(prompt(`Digite um numero: `))
+// let numeroArray: number[] = []
+// if (numero == 0) {
+//     alert(`Programa encerrado!`)
+// } else {
+//     while (numero !== 0) {
+//         if (numero % 3 ==0) {
+//             numeroArray.push(numero)
+//         }
+//         numero = Number(prompt(`Digite um numero: `))
+//     }
+//     let soma: number = 0
+//     for (let index = 0; index < numeroArray.length; index++) {
+//         soma += numeroArray[index]
+//     }
+//     let media: number = soma / numeroArray.length
+//     alert(`A media dos numeros divisiveis por 3 informados e de: ${media}`)
+// }
+// Exercício 21
+// Exercício 22
+// Exercício 23
+// Exercício 24
+var numero1 = Number(prompt("Digite um numero"));
+var numerosArray = [];
+var paresArray = [];
+var imparesArray = [];
+var numero2;
+if (numero1 == 0) {
+    alert("Programa encerrado!");
+}
+else {
+    while (numero2 !== 0) {
+        numero2 = prompt("Digite um numero: ");
+    }
+    var index = numero1;
+    while (index <= numero2) {
+        if (index % 2 == 0) {
+            paresArray.push(index);
+        }
+        else if (index % 2 == 1) {
+            imparesArray.push(index);
+        }
+        index++;
+    }
+    alert("Foram digitados ".concat(paresArray.length, " numeros pares.\nForam digitados ").concat(imparesArray.length, " numeros impares."));
+}
